70b8b8a9d3af92dab3ae13e3f96d468c30e4a983
priyankacute <bhavanimurugesh2000@gmail.com>
2023-07-17 15:47:00 +0530

    add new

---
+++ main.js
@@ -0,0 +1,10 @@
+var n =prompt("enter the number to find factorial")
+var fact=1;
+ for(let i=1; i<=n; i++){
+						fact=i*fact;
+						document.write(fact);
+						document.write("<br>");
+						}
+						
+ 
+	 
